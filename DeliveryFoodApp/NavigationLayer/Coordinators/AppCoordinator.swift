475d713885cb89a88e52366db3d332726cacade6
Ilfat <salakhoff.ilfat@gmail.com>
2024-05-07 22:40:35 +0300

    added factory

--- DeliveryFoodApp/NavigationLayer/Coordinators/AppCoordinator.swift
+++ DeliveryFoodApp/NavigationLayer/Coordinators/AppCoordinator.swift
@@ -3,15 +3,20 @@ import UIKit
 /// Создание координатора, который будет использоваться со сценой
 final class AppCoordinator: Coordinator {
     
+    // MARK: Properties
+    
     private let userStorage = UserStorage.shared
+    private let factory = SceneFactory.self
     
     /// Показывается, какой VC будет показан
     override func start() {
-        userStorage.passedOnboarding ? showMainFlow() : showMainFlow()
+        showOnboardingFlow()
+//        userStorage.passedOnboarding ?
+//        showMainFlow() : showOnboardingFlow()
     }
     
     override func finish() {
-        print("AppCoordinator свою работу")
+        print("AppCoordinator закончил свою работу")
     }
 }
 
@@ -21,75 +26,17 @@ extension AppCoordinator {
     /// Реализация OnboardingFlow
     func showOnboardingFlow() {
         guard let navigationController else { return }
-        
-        let onboardingCoordinator = OnboardingCoordinator(
-            type: .onboarding,
+        factory.makeOnboardingFlow(
+            coordinator: self,
             navigationController: navigationController,
-            finishDelegate: self
-        )
-        
-        addChildCoordinator(onboardingCoordinator)
-        
-        onboardingCoordinator.start()
+            finishDelegate: self)
     }
     
     /// Реализация MainFlow
     func showMainFlow() {
         guard let navigationController else { return }
         
-        let homeNavigationController = UINavigationController()
-        let homeCoordinator = HomeCoordinator(type: .home, navigationController: homeNavigationController)
-        homeNavigationController.tabBarItem = UITabBarItem(
-            title: "Home",
-            image: UIImage(systemName: ""),
-            tag: 0
-        )
-        homeCoordinator.finishDelegate = self
-        homeCoordinator.start()
-        
-        let orderNavigationController = UINavigationController()
-        let orderCoordinator = OrderCoordinator(type: .order, navigationController: orderNavigationController)
-        orderNavigationController.tabBarItem = UITabBarItem(
-            title: "Order",
-            image: UIImage(systemName: ""),
-            tag: 0
-        )
-        orderCoordinator.finishDelegate = self
-        orderCoordinator.start()
-        
-        let listNavigationController = UINavigationController()
-        let listCoordinator = ListCoordinator(type: .list, navigationController: listNavigationController)
-        listNavigationController.tabBarItem = UITabBarItem(
-            title: "My List",
-            image: UIImage(systemName: ""),
-            tag: 0
-        )
-        listCoordinator.finishDelegate = self
-        listCoordinator.start()
-        
-        let profileNavigationController = UINavigationController()
-        let profileCoordinator = ProfileCoordinator(type: .profile, navigationController: profileNavigationController)
-        profileNavigationController.tabBarItem = UITabBarItem(
-            title: "Profile",
-            image: UIImage(systemName: ""),
-            tag: 0
-        )
-        profileCoordinator.finishDelegate = self
-        profileCoordinator.start()
-        
-        addChildCoordinator(homeCoordinator)
-        addChildCoordinator(orderCoordinator)
-        addChildCoordinator(listCoordinator)
-        addChildCoordinator(profileCoordinator)
-        
-        let tabBarControllers = [
-            homeNavigationController,
-            orderNavigationController,
-            listNavigationController,
-            profileNavigationController
-        ]
-        
-        let tabBarController = TabBarController(tabBarControllers: tabBarControllers)
+        let tabBarController = factory.makeMainFlow(coordinator: self, finishDelegate: self)
         
         navigationController.pushViewController(tabBarController, animated: false)
     }
