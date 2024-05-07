88a3d290d79e100282747a6a56a5d491eb5e0255
Ilfat <salakhoff.ilfat@gmail.com>
2024-05-07 13:26:12 +0300

    add new fonts

--- DeliveryFoodApp/ViewController.swift
+++ DeliveryFoodApp/ViewController.swift
@@ -4,19 +4,5 @@ final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        
-        let title = UILabel()
-        view.addSubview(title)
-        title.text = "PIZDA"
-        
-        title.translatesAutoresizingMaskIntoConstraints = false
-        
-        title.textColor = .black
-        
-        NSLayoutConstraint.activate([
-            title.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            title.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
     }
 }
