b263ae97aa098eb64942ea2ddf1a0fd8a7eb6997
Ilfat <salakhoff.ilfat@gmail.com>
2024-05-07 11:51:32 +0300

    add new fonts Roboto

--- DeliveryFoodApp/ViewController.swift
+++ DeliveryFoodApp/ViewController.swift
@@ -4,6 +4,19 @@ final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .white
+        
+        let title = UILabel()
+        view.addSubview(title)
+        title.text = "PIZDA"
+        
+        title.translatesAutoresizingMaskIntoConstraints = false
+        
+        title.textColor = .black
+        
+        NSLayoutConstraint.activate([
+            title.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            title.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
